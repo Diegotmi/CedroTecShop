243731086d168cb47dee03cafd7b2c1d387fa959
Diego <dihdhstmi@gmail.com>
2017-11-22 22:00:30 -0200

    Adiciona modelo computador

---
+++ CedroTecShop/Models/Computador.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CedroTecShop.Models
+{
+    public class Computador
+    {
+        public int ID { get; set; }
+        public string Descricao { get; set; }
+        public decimal Preco { get; set; }
+
+    }
+}
