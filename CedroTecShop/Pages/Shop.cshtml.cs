209b5136d4c24cce44406cff951d259f0e627c7f
Diego <dihdhstmi@gmail.com>
2017-11-23 22:19:25 -0200

    Carrega lista de computadores na pagina shop

--- CedroTecShop/Pages/Shop.cshtml.cs
+++ CedroTecShop/Pages/Shop.cshtml.cs
@@ -2,16 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CedroTecShop.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace CedroTecShop.Pages
 {
     public class ShopModel : PageModel
     {
-        public void OnGet()
-        {
+        private readonly CedroTecShop.Models.ShopContext _context;
+        private IList<Computador> Carrinho { get; set; }
 
+        public ShopModel(CedroTecShop.Models.ShopContext context)
+        {
+            _context = context;
+        }
+
+
+        public IList<Computador> Computadores { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            Computadores = await _context.Computador.ToListAsync();
         }
     }
 }
